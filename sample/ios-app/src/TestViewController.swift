5ca2acc30f963e104ceed2b122de0c8ad12609ee
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-11-11 17:21:45 +0700

    ios compilation fix

--- sample/ios-app/src/TestViewController.swift
+++ sample/ios-app/src/TestViewController.swift
@@ -7,38 +7,9 @@ import MultiPlatformLibrary
 import MultiPlatformLibraryUnits
 
 class TestViewController: UIViewController {
-    
-    @IBOutlet private var tableView: UITableView!
-    private var dataSource: UITableViewDataSource!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let testing = Testing(unitFactory: self)
         
-        let dataSource = FlatUnitTableViewDataSource()
-        dataSource.setup(for: tableView)
-        
-        let units = testing.getUnits() as! [UITableViewCellUnitProtocol]
-        dataSource.units = units
-        
-        tableView.tableFooterView = UIView()
-        self.dataSource = dataSource
-    }
-}
-
-extension TestViewController: TestingUnitFactory {
-    func createSimpleUnit(id: Int64, title: String, itemData: ItemData?) -> UnitItem {
-        // without R.swift
-//        return UITableViewCellUnit<SimpleCell>(
-//            data: SimpleCell.CellModel(id: id, title: title),
-//            configurator: nil
-//        )
-        // with R.swift
-        return UITableViewCellUnit<SimpleCell>(
-            data: SimpleCell.CellModel(id: id, title: title),
-            reusable: R.nib.simpleCell,
-            configurator: nil
-        )
     }
 }
