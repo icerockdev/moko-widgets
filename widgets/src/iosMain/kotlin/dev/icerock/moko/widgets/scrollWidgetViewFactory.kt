eeddb6370dd7bced241724a69033d7d79ad6520c
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-11-12 15:52:35 +0700

    ScrollWidget base implementation for ios

--- widgets/src/iosMain/kotlin/dev/icerock/moko/widgets/scrollWidgetViewFactory.kt
+++ widgets/src/iosMain/kotlin/dev/icerock/moko/widgets/scrollWidgetViewFactory.kt
@@ -1,9 +1,40 @@
 package dev.icerock.moko.widgets
 
 import dev.icerock.moko.widgets.core.VFC
-import platform.UIKit.UIView
+import kotlinx.cinterop.readValue
+import platform.CoreGraphics.CGRectZero
+import platform.UIKit.UIColor
+import platform.UIKit.UILabel
+import platform.UIKit.UIScrollView
+import platform.UIKit.addSubview
+import platform.UIKit.backgroundColor
+import platform.UIKit.bottomAnchor
+import platform.UIKit.heightAnchor
+import platform.UIKit.leadingAnchor
+import platform.UIKit.leftAnchor
+import platform.UIKit.topAnchor
+import platform.UIKit.trailingAnchor
+import platform.UIKit.translatesAutoresizingMaskIntoConstraints
+import platform.UIKit.widthAnchor
 
-actual var scrollWidgetViewFactory: VFC<ScrollWidget> = { _, _ ->
-    // TODO add factory implementation
-    UIView()
+actual var scrollWidgetViewFactory: VFC<ScrollWidget> = { viewController, widget ->
+    // TODO add styles support
+    val style = widget.style
+
+    val scrollView = UIScrollView(frame = CGRectZero.readValue())
+    scrollView.translatesAutoresizingMaskIntoConstraints = false
+
+    val childView = widget.child.buildView(viewController)
+    childView.translatesAutoresizingMaskIntoConstraints = false
+    
+    with(scrollView) {
+        addSubview(childView)
+
+        childView.topAnchor.constraintEqualToAnchor(topAnchor).active = true
+        childView.leftAnchor.constraintEqualToAnchor(leftAnchor).active = true
+        childView.widthAnchor.constraintEqualToAnchor(widthAnchor).active = true
+        bottomAnchor.constraintEqualToAnchor(childView.bottomAnchor).active = true
+    }
+
+    scrollView
 }
