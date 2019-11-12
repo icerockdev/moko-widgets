99d3b79a0dab1ba2c7d4622909b2669cae5a507b
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-11-12 17:30:02 +0700

    FlatAlertWidget base implementation for ios

--- widgets/src/iosMain/kotlin/dev/icerock/moko/widgets/flatAlertWidgetViewFactory.kt
+++ widgets/src/iosMain/kotlin/dev/icerock/moko/widgets/flatAlertWidgetViewFactory.kt
@@ -5,9 +5,93 @@
 package dev.icerock.moko.widgets
 
 import dev.icerock.moko.widgets.core.VFC
+import dev.icerock.moko.widgets.core.bind
+import dev.icerock.moko.widgets.utils.localized
+import dev.icerock.moko.widgets.utils.setEventHandler
+import kotlinx.cinterop.readValue
+import platform.CoreGraphics.CGRectZero
+import platform.UIKit.NSTextAlignmentCenter
+import platform.UIKit.UIButton
+import platform.UIKit.UIButtonTypeRoundedRect
+import platform.UIKit.UIControlEventTouchUpInside
+import platform.UIKit.UIControlStateNormal
+import platform.UIKit.UILabel
 import platform.UIKit.UIView
+import platform.UIKit.addSubview
+import platform.UIKit.bottomAnchor
+import platform.UIKit.leadingAnchor
+import platform.UIKit.topAnchor
+import platform.UIKit.trailingAnchor
+import platform.UIKit.translatesAutoresizingMaskIntoConstraints
 
-actual var flatAlertWidgetViewFactory: VFC<FlatAlertWidget> = { _, _ ->
-    // TODO add factory implementation
-    UIView()
+actual var flatAlertWidgetViewFactory: VFC<FlatAlertWidget> = { viewController, widget ->
+    // TODO add styles support
+    val style = widget.style
+
+    val container = UIView(frame = CGRectZero.readValue()).apply {
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    val titleLabel = widget.title?.let { title ->
+        val titleLabel = UILabel(frame = CGRectZero.readValue()).apply {
+            translatesAutoresizingMaskIntoConstraints = false
+            textAlignment = NSTextAlignmentCenter
+        }
+
+        title.bind { titleLabel.text = it?.localized() }
+
+        titleLabel
+    }
+
+    val messageLabel = UILabel(frame = CGRectZero.readValue()).apply {
+        translatesAutoresizingMaskIntoConstraints = false
+        textAlignment = NSTextAlignmentCenter
+    }
+    widget.message.bind { messageLabel.text = it?.localized() }
+
+    val button = widget.buttonText?.let { text ->
+        val button = UIButton.buttonWithType(buttonType = UIButtonTypeRoundedRect).apply {
+            translatesAutoresizingMaskIntoConstraints = false
+        }
+
+        text.bind { button.setTitle(it?.localized(), forState = UIControlStateNormal) }
+
+        button.setEventHandler(UIControlEventTouchUpInside) {
+            widget.onTap?.invoke()
+        }
+
+        button
+    }
+
+    with(container) {
+        addSubview(messageLabel)
+
+        messageLabel.leadingAnchor.constraintEqualToAnchor(leadingAnchor).active = true
+        messageLabel.trailingAnchor.constraintEqualToAnchor(trailingAnchor).active = true
+
+        if (titleLabel != null) {
+            addSubview(titleLabel)
+
+            titleLabel.leadingAnchor.constraintEqualToAnchor(leadingAnchor).active = true
+            titleLabel.trailingAnchor.constraintEqualToAnchor(trailingAnchor).active = true
+
+            titleLabel.topAnchor.constraintEqualToAnchor(topAnchor).active = true
+            messageLabel.topAnchor.constraintEqualToAnchor(titleLabel.bottomAnchor).active = true
+        } else {
+            messageLabel.topAnchor.constraintEqualToAnchor(topAnchor).active = true
+        }
+
+        if (button != null) {
+            addSubview(button)
+
+            button.leadingAnchor.constraintEqualToAnchor(leadingAnchor).active = true
+            button.trailingAnchor.constraintEqualToAnchor(trailingAnchor).active = true
+            button.topAnchor.constraintEqualToAnchor(messageLabel.bottomAnchor).active = true
+            button.bottomAnchor.constraintEqualToAnchor(bottomAnchor).active = true
+        } else {
+            messageLabel.bottomAnchor.constraintEqualToAnchor(bottomAnchor).active = true
+        }
+    }
+
+    container
 }
