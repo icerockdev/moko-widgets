62c5f1757e8e0a98df569f436267324b3b84cd57
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-11-12 16:42:09 +0700

    InputWidget base implementation for ios

--- widgets/src/iosMain/kotlin/dev/icerock/moko/widgets/inputWidgetViewFactory.kt
+++ widgets/src/iosMain/kotlin/dev/icerock/moko/widgets/inputWidgetViewFactory.kt
@@ -5,9 +5,34 @@
 package dev.icerock.moko.widgets
 
 import dev.icerock.moko.widgets.core.VFC
-import platform.UIKit.UIView
+import dev.icerock.moko.widgets.core.bind
+import dev.icerock.moko.widgets.utils.setEventHandler
+import kotlinx.cinterop.readValue
+import platform.CoreGraphics.CGRectZero
+import platform.UIKit.UIControlEventEditingChanged
+import platform.UIKit.UIControlEventValueChanged
+import platform.UIKit.UITextBorderStyle
+import platform.UIKit.UITextField
+import platform.UIKit.translatesAutoresizingMaskIntoConstraints
 
-actual var inputWidgetViewFactory: VFC<InputWidget> = { _, _ ->
-    // TODO add factory implementation
-    UIView()
+actual var inputWidgetViewFactory: VFC<InputWidget> = { viewController, widget ->
+    // TODO add styles support
+    val style = widget.style
+
+    val textField = UITextField(frame = CGRectZero.readValue())
+    textField.translatesAutoresizingMaskIntoConstraints = false
+    textField.borderStyle = UITextBorderStyle.UITextBorderStyleRoundedRect
+
+    widget.field.data.bind { textField.text = it }
+
+    textField.setEventHandler(UIControlEventEditingChanged) {
+        val text = textField.text
+        val currentValue = widget.field.data.value
+
+        if (currentValue != text) {
+            widget.field.data.value = text.orEmpty()
+        }
+    }
+
+    textField
 }
