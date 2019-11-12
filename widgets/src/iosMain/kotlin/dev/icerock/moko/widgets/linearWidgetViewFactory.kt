9bfbc3a9112887eba895d755c0128c1a02903a9e
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-11-12 16:42:19 +0700

    LinearWidget base implementation for ios

--- widgets/src/iosMain/kotlin/dev/icerock/moko/widgets/linearWidgetViewFactory.kt
+++ widgets/src/iosMain/kotlin/dev/icerock/moko/widgets/linearWidgetViewFactory.kt
@@ -5,9 +5,47 @@
 package dev.icerock.moko.widgets
 
 import dev.icerock.moko.widgets.core.VFC
+import kotlinx.cinterop.readValue
+import platform.CoreGraphics.CGRectZero
 import platform.UIKit.UIView
+import platform.UIKit.addSubview
+import platform.UIKit.bottomAnchor
+import platform.UIKit.leadingAnchor
+import platform.UIKit.topAnchor
+import platform.UIKit.trailingAnchor
+import platform.UIKit.translatesAutoresizingMaskIntoConstraints
 
-actual var linearWidgetViewFactory: VFC<LinearWidget> = { _, _ ->
-    // TODO add factory implementation
-    UIView()
+actual var linearWidgetViewFactory: VFC<LinearWidget> = { viewController, widget ->
+    // TODO add styles support
+    val style = widget.style
+
+    val container = UIView(frame = CGRectZero.readValue())
+    container.translatesAutoresizingMaskIntoConstraints = false
+
+    var lastChildView: UIView? = null
+    widget.childs.forEach { widget ->
+        val childView = widget.buildView(viewController)
+        childView.translatesAutoresizingMaskIntoConstraints = false
+
+        with(container) {
+            container.addSubview(childView)
+
+            val lastCV = lastChildView
+            if (lastCV != null) {
+                childView.topAnchor.constraintEqualToAnchor(lastCV.bottomAnchor).active = true
+            } else {
+                childView.topAnchor.constraintEqualToAnchor(topAnchor).active = true
+            }
+            childView.leadingAnchor.constraintEqualToAnchor(leadingAnchor).active = true
+            childView.trailingAnchor.constraintEqualToAnchor(trailingAnchor).active = true
+        }
+
+        lastChildView = childView
+    }
+
+    lastChildView?.run {
+        container.bottomAnchor.constraintEqualToAnchor(bottomAnchor).active = true
+    }
+
+    container
 }
