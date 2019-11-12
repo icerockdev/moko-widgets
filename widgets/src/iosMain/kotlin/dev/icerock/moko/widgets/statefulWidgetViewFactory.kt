8718a6b09df1ba8cb1a549094e962392a5b00d3a
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-11-12 16:42:41 +0700

    StatefulWidget base implementation for ios

--- widgets/src/iosMain/kotlin/dev/icerock/moko/widgets/statefulWidgetViewFactory.kt
+++ widgets/src/iosMain/kotlin/dev/icerock/moko/widgets/statefulWidgetViewFactory.kt
@@ -4,10 +4,57 @@
 
 package dev.icerock.moko.widgets
 
+import dev.icerock.moko.mvvm.State
 import dev.icerock.moko.widgets.core.VFC
+import dev.icerock.moko.widgets.core.Widget
+import kotlinx.cinterop.readValue
+import platform.CoreGraphics.CGRectZero
 import platform.UIKit.UIView
+import platform.UIKit.addSubview
+import platform.UIKit.bottomAnchor
+import platform.UIKit.hidden
+import platform.UIKit.leftAnchor
+import platform.UIKit.topAnchor
+import platform.UIKit.translatesAutoresizingMaskIntoConstraints
+import platform.UIKit.widthAnchor
 
-actual var statefulWidgetViewFactory: VFC<StatefulWidget<*, *>> = { _, _ ->
-    // TODO add factory implementation
-    UIView()
+actual var statefulWidgetViewFactory: VFC<StatefulWidget<*, *>> = { viewController, widget ->
+    // TODO add styles support
+    val style = widget.style
+
+    val container = UIView(frame = CGRectZero.readValue())
+    container.translatesAutoresizingMaskIntoConstraints = false
+
+    listOf<Widget>(
+        widget.dataWidget,
+        widget.emptyWidget,
+        widget.loadingWidget,
+        widget.errorWidget
+    ).forEach { childWidget ->
+        val childView = childWidget.buildView(viewController)
+        childView.translatesAutoresizingMaskIntoConstraints = false
+
+        with(container) {
+            addSubview(childView)
+
+            childView.topAnchor.constraintEqualToAnchor(topAnchor).active = true
+            childView.leftAnchor.constraintEqualToAnchor(leftAnchor).active = true
+            childView.widthAnchor.constraintEqualToAnchor(widthAnchor).active = true
+            childView.bottomAnchor.constraintEqualToAnchor(bottomAnchor).active = true
+        }
+
+        fun updateState(state: State<*, *>) {
+            childView.hidden = when (state) {
+                is State.Data -> childWidget == widget.dataWidget
+                is State.Empty -> childWidget == widget.emptyWidget
+                is State.Error -> childWidget == widget.errorWidget
+                is State.Loading -> childWidget == widget.loadingWidget
+            }.not()
+        }
+
+        updateState(widget.stateLiveData.value)
+        widget.stateLiveData.addObserver { updateState(it) }
+    }
+
+    container
 }
