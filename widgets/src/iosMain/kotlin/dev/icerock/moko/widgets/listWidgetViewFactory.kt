4ed95a2000d0333c6b97e9fa75ae637c641e8818
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-11-22 09:59:16 +0700

    ListWidget onRefresh, onReachEnd for iOS

--- widgets/src/iosMain/kotlin/dev/icerock/moko/widgets/listWidgetViewFactory.kt
+++ widgets/src/iosMain/kotlin/dev/icerock/moko/widgets/listWidgetViewFactory.kt
@@ -4,20 +4,24 @@
 
 package dev.icerock.moko.widgets
 
+import dev.icerock.moko.units.TableUnitItem
 import dev.icerock.moko.units.UnitTableViewDataSource
 import dev.icerock.moko.widgets.core.VFC
 import dev.icerock.moko.widgets.core.bind
 import dev.icerock.moko.widgets.utils.applyBackground
+import dev.icerock.moko.widgets.utils.setEventHandler
 import dev.icerock.moko.widgets.utils.toEdgeInsets
 import kotlinx.cinterop.readValue
 import platform.CoreGraphics.CGRectZero
+import platform.UIKit.UIControlEventValueChanged
+import platform.UIKit.UIRefreshControl
 import platform.UIKit.UITableView
 import platform.UIKit.UITableViewAutomaticDimension
+import platform.UIKit.UITableViewCell
 import platform.UIKit.UITableViewStyle
 import platform.UIKit.translatesAutoresizingMaskIntoConstraints
 
 actual var listWidgetViewFactory: VFC<ListWidget> = { viewController, widget ->
-    // TODO add styles support
     val style = widget.style
 
     val tableView = UITableView(
@@ -37,9 +41,45 @@ actual var listWidgetViewFactory: VFC<ListWidget> = { viewController, widget ->
         style.padding?.toEdgeInsets()?.also {
             contentInset = it
         }
+
+        widget.onRefresh?.let { onRefresh ->
+            refreshControl = UIRefreshControl().apply {
+                setEventHandler(UIControlEventValueChanged) {
+                    onRefresh { endRefreshing() }
+                }
+            }
+        }
+
+        // TODO reversed apply
     }
 
-    widget.items.bind { unitDataSource.unitItems = it }
+    widget.items.bind { items ->
+        unitDataSource.unitItems = if (widget.onReachEnd != null) {
+            items.observedEnd(widget.onReachEnd)
+        } else {
+            items
+        }
+    }
 
     tableView
+}
+
+private fun List<TableUnitItem>.observedEnd(onReachEnd: () -> Unit): List<TableUnitItem> {
+    if (this.isEmpty()) return this
+
+    val lastWrapped = TableUnitItemWrapper(
+        item = this.last(),
+        onBind = onReachEnd
+    )
+    return this.dropLast(1).plus(lastWrapped)
+}
+
+class TableUnitItemWrapper(
+    private val item: TableUnitItem,
+    private val onBind: () -> Unit
+) : TableUnitItem by item {
+    override fun bind(cell: UITableViewCell) {
+        item.bind(cell)
+        onBind()
+    }
 }
