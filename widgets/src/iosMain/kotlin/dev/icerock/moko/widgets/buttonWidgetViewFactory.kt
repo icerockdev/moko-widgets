ac585c0dea437d28a4c48463bc70d107316bd39e
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-11-22 18:56:23 +0700

    buttons state background rework

--- widgets/src/iosMain/kotlin/dev/icerock/moko/widgets/buttonWidgetViewFactory.kt
+++ widgets/src/iosMain/kotlin/dev/icerock/moko/widgets/buttonWidgetViewFactory.kt
@@ -7,12 +7,16 @@ package dev.icerock.moko.widgets
 import dev.icerock.moko.graphics.toUIColor
 import dev.icerock.moko.widgets.core.VFC
 import dev.icerock.moko.widgets.core.bind
-import dev.icerock.moko.widgets.utils.applyBackground
+import dev.icerock.moko.widgets.style.background.StateBackground
 import dev.icerock.moko.widgets.utils.applyTextStyle
+import dev.icerock.moko.widgets.utils.caLayer
+import dev.icerock.moko.widgets.utils.displayLink
 import dev.icerock.moko.widgets.utils.setEventHandler
-import dev.icerock.moko.widgets.utils.UIStyledButton
+import kotlinx.cinterop.useContents
+import platform.CoreGraphics.CGRectMake
+import platform.QuartzCore.CATransaction
 import platform.UIKit.UIButton
-import platform.UIKit.UIButtonTypeRoundedRect
+import platform.UIKit.UIButtonTypeSystem
 import platform.UIKit.UIControlEventTouchUpInside
 import platform.UIKit.UIControlStateNormal
 import platform.UIKit.UILayoutConstraintAxisHorizontal
@@ -26,12 +30,19 @@ actual var buttonWidgetViewFactory: VFC<ButtonWidget> = { viewController, widget
     val style = widget.style
 
 
-    val button = UIStyledButton().apply {
+    val button = UIButton.buttonWithType(UIButtonTypeSystem).apply {
         translatesAutoresizingMaskIntoConstraints = false
-        setContentHuggingPriority(UILayoutPriorityRequired, forAxis = UILayoutConstraintAxisHorizontal)
-        setContentHuggingPriority(UILayoutPriorityRequired, forAxis = UILayoutConstraintAxisVertical)
+        setContentHuggingPriority(
+            UILayoutPriorityRequired,
+            forAxis = UILayoutConstraintAxisHorizontal
+        )
+        setContentHuggingPriority(
+            UILayoutPriorityRequired,
+            forAxis = UILayoutConstraintAxisVertical
+        )
+
         applyStateBackground(style.background)
-        
+
         titleLabel?.applyTextStyle(style.textStyle)
 
         style.textStyle.color?.also {
@@ -49,4 +60,57 @@ actual var buttonWidgetViewFactory: VFC<ButtonWidget> = { viewController, widget
     }
 
     button
+}
+
+fun UIButton.applyStateBackground(background: StateBackground?) {
+    if (background == null) return
+
+    adjustsImageWhenDisabled = false
+    adjustsImageWhenHighlighted = false
+
+    val normalBg = background.normal.caLayer().also {
+        layer.addSublayer(it)
+    }
+    val disabledBg = background.disabled.caLayer().also {
+        layer.addSublayer(it)
+    }
+    val pressedBg = background.pressed.caLayer().also {
+        layer.addSublayer(it)
+    }
+
+    fun updateLayers() {
+        if (!isEnabled()) {
+            disabledBg.opacity = 1.0f
+            normalBg.opacity = 0f
+            pressedBg.opacity = 0f
+            return
+        }
+
+        if (isHighlighted()) {
+            pressedBg.opacity = 1.0f
+            normalBg.opacity = 0f
+        } else {
+            normalBg.opacity = 1.0f
+            pressedBg.opacity = 0f
+        }
+        disabledBg.opacity = 0f
+    }
+
+    updateLayers()
+
+    // FIXME memoryleak, perfomance problem !!!
+    displayLink {
+        val (width, height) = layer.bounds.useContents { size.width to size.height }
+
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+
+        normalBg.frame = CGRectMake(0.0, 0.0, width, height)
+        disabledBg.frame = CGRectMake(0.0, 0.0, width, height)
+        pressedBg.frame = CGRectMake(0.0, 0.0, width, height)
+
+        updateLayers()
+
+        CATransaction.commit()
+    }
 }
--- widgets/src/iosMain/kotlin/dev/icerock/moko/widgets/utils/UIControlExt.kt
+++ widgets/src/iosMain/kotlin/dev/icerock/moko/widgets/utils/UIControlExt.kt
@@ -4,21 +4,14 @@
 
 package dev.icerock.moko.widgets.utils
 
-import dev.icerock.moko.widgets.core.View
-import dev.icerock.moko.widgets.core.ViewFactoryContext
-import dev.icerock.moko.widgets.core.Widget
-import dev.icerock.moko.widgets.style.background.StateBackground
-import kotlinx.cinterop.*
-import platform.CoreGraphics.CGRect
-import platform.CoreGraphics.CGRectMake
-import platform.CoreGraphics.CGRectZero
-import platform.Foundation.NSCoder
+import kotlinx.cinterop.ObjCAction
+import kotlinx.cinterop.cstr
 import platform.Foundation.NSDefaultRunLoopMode
 import platform.Foundation.NSRunLoop
 import platform.Foundation.NSSelectorFromString
 import platform.QuartzCore.CADisplayLink
-import platform.QuartzCore.CALayer
-import platform.UIKit.*
+import platform.UIKit.UIControl
+import platform.UIKit.UIControlEvents
 import platform.darwin.NSObject
 import platform.objc.OBJC_ASSOCIATION_RETAIN
 import platform.objc.objc_setAssociatedObject
@@ -62,66 +55,5 @@ class LambdaTarget(val lambda: () -> Unit) : NSObject() {
     @ObjCAction
     fun displayLink(link: CADisplayLink) {
         lambda()
-    }
-}
-
-class UIStyledButton: UIButton(frame = CGRectZero.readValue()) {
-    private var normalBg: CALayer? = null
-    private var disabledBg: CALayer? = null
-    private var pressedBg: CALayer? = null
-    
-    fun applyStateBackground(background: StateBackground?) {
-        if (background == null) return
-        adjustsImageWhenDisabled = false
-        adjustsImageWhenHighlighted = false
-        normalBg = background.normal.caLayer().also {
-            layer.addSublayer(it)
-        }
-        disabledBg = background.disabled.caLayer().also {
-            layer.addSublayer(it)
-        }
-        pressedBg = background.pressed.caLayer().also {
-            layer.addSublayer(it)
-        }
-
-        updateLayers()
-    }
-
-    override fun layoutSublayersOfLayer(layer: CALayer) {
-        super.layoutSublayersOfLayer(layer)
-        val (width, height) = layer.bounds.useContents { size.width to size.height }
-        normalBg?.frame = CGRectMake(0.0, 0.0, width, height)
-        disabledBg?.frame = CGRectMake(0.0, 0.0, width, height)
-        pressedBg?.frame = CGRectMake(0.0, 0.0, width, height)
-    }
-
-    private fun updateLayers() {
-
-        if (!isEnabled()) {
-            disabledBg?.opacity = 1.0f
-            normalBg?.opacity = 0f
-            pressedBg?.opacity = 0f
-            return
-        }
-
-        if (isHighlighted()) {
-            pressedBg?.opacity = 1.0f
-            normalBg?.opacity = 0f
-        } else {
-            normalBg?.opacity = 1.0f
-            pressedBg?.opacity = 0f
-        }
-        disabledBg?.opacity = 0f
-
-    }
-
-    override fun setEnabled(enabled: Boolean) {
-        super.setEnabled(enabled)
-        updateLayers()
-    }
-
-    override fun setHighlighted(highlighted: Boolean) {
-        super.setHighlighted(highlighted)
-        updateLayers()
     }
 }
