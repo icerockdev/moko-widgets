d2a10db7e101a3f9c65a969147f7d3ce7030448d
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-12-08 23:03:59 +0700

    up version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -10,7 +10,7 @@ object Versions {
     }
 
     const val kotlin = "1.3.61"
-    private const val mokoWidgets = "0.1.0-dev-5"
+    private const val mokoWidgets = "0.1.0-dev-6"
     private const val mokoResources = "0.6.0-dev-1"
 
     object Plugins {
