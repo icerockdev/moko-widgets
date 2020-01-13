671859c2f405af18ac11e8704578110952b9e9fd
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-01-13 11:52:44 +0700

    #3 completed navigation API #6 result routing API

--- sample/mpp-library/src/commonMain/kotlin/Playground.kt
+++ sample/mpp-library/src/commonMain/kotlin/Playground.kt
@@ -7,12 +7,19 @@ import dev.icerock.moko.mvvm.viewmodel.ViewModel
 import dev.icerock.moko.parcelize.Parcelable
 import dev.icerock.moko.parcelize.Parcelize
 import dev.icerock.moko.widgets.core.Theme
+import dev.icerock.moko.widgets.core.Widget
 import dev.icerock.moko.widgets.screen.Args
 import dev.icerock.moko.widgets.screen.NavigationBar
 import dev.icerock.moko.widgets.screen.NavigationItem
 import dev.icerock.moko.widgets.screen.Screen
 import dev.icerock.moko.widgets.screen.WidgetScreen
+import dev.icerock.moko.widgets.style.view.SizeSpec
+import dev.icerock.moko.widgets.style.view.WidgetSize
+import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KClass
+import kotlin.reflect.KProperty
+
+// LIB SIDE
 
 class ScreenFactory<Arg : Args, T : Screen<Arg>>
 
@@ -41,54 +48,154 @@ class TypedScreenDesc<Arg : Args, T : Screen<Arg>>(
     override fun instantiate(): T = factory.invoke(ScreenFactory())
 }
 
+abstract class Screen<Arg : Args>() {
+    fun <T : Any> createEventsDispatcher(): EventsDispatcher<T> {
+        TODO()
+    }
+
+    inline fun <reified VM : ViewModel, Key : Any> getViewModel(key: Key, crossinline viewModelFactory: () -> VM): VM {
+        TODO()
+    }
+}
+
+class RouteHandlerProperty<T> : ReadOnlyProperty<Screen<*>, RouteHandler<T>> {
+    override fun getValue(thisRef: Screen<*>, property: KProperty<*>): RouteHandler<T> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
+fun <T> Screen<*>.registerRouteHandler(handler: (T?) -> Unit): RouteHandlerProperty<T> {
+    TODO()
+}
+
+abstract class WidgetScreen<Arg : Args>() : Screen<Arg>() {
+    open val isDismissKeyboardOnTap: Boolean = false
+
+    open val isKeyboardResizeContent: Boolean = false
+
+    abstract fun createContentWidget(): Widget<WidgetSize.Const<SizeSpec.AsParent, SizeSpec.AsParent>>
+}
+
+interface Resultable<R : Parcelable> {
+    val screenResult: R?
+}
+
+interface Route<T> {
+    fun route(source: Screen<*>, arg: T)
+}
+
+fun Route<Unit>.route(source: Screen<*>) {
+    route(source = source, arg = Unit)
+}
+
+interface RouteHandler<T>
+
+interface RouteWithResult<Arg, T> {
+    fun route(
+        source: Screen<*>,
+        arg: Arg,
+        handler: RouteHandler<T>
+    )
+}
+
+fun <T> RouteWithResult<Unit, T>.route(source: Screen<*>, handler: RouteHandler<T>) {
+    route(source = source, arg = Unit, handler = handler)
+}
+
+fun ScreenFactory<Args.Empty, NavigationScreen<*>>.createRouter(): NavigationScreen.Router {
+    println(this)
+    return NavigationScreen.Router()
+}
+
+class NavigationScreen<T>(
+    initialScreen: TypedScreenDesc<Args.Empty, T>,
+    router: Router
+) : Screen<Args.Empty>() where T : Screen<Args.Empty>, T : NavigationItem {
+
+    class Router() {
+        private fun <T, Arg : Args, S> createRouteInternal(
+            destination: TypedScreenDesc<Arg, S>,
+            inputMapper: (T) -> Arg
+        ): Route<T> where S : Screen<Arg>, S : NavigationItem = TODO()
+
+        fun <S> createRoute(
+            destination: TypedScreenDesc<Args.Empty, S>
+        ): Route<Unit> where S : Screen<Args.Empty>, S : NavigationItem = createRouteInternal(
+            destination = destination,
+            inputMapper = { Args.Empty }
+        )
+
+        fun <T, P : Parcelable, S> createRoute(
+            destination: TypedScreenDesc<Args.Parcel<P>, S>,
+            inputMapper: (T) -> P
+        ): Route<T> where S : Screen<Args.Parcel<P>>, S : NavigationItem = createRouteInternal(
+            destination = destination,
+            inputMapper = { Args.Parcel(inputMapper(it)) }
+        )
+
+        private fun <IT, Arg : Args, OT, R : Parcelable, S> createResultRouteInternal(
+            destination: TypedScreenDesc<Arg, S>,
+            inputMapper: (IT) -> Arg,
+            outputMapper: (R) -> OT
+        ): RouteWithResult<IT, OT> where S : Screen<Arg>, S : Resultable<R>, S : NavigationItem = TODO()
+
+        fun <OT, R : Parcelable, S> createResultRoute(
+            destination: TypedScreenDesc<Args.Empty, S>,
+            outputMapper: (R) -> OT
+        ): RouteWithResult<Unit, OT> where S : Screen<Args.Empty>, S : Resultable<R>, S : NavigationItem = TODO()
+
+        fun <IT, P : Parcelable, OT, R : Parcelable, S> createResultRoute(
+            destination: TypedScreenDesc<Args.Parcel<P>, S>,
+            inputMapper: (IT) -> P,
+            outputMapper: (R) -> OT
+        ): RouteWithResult<IT, OT> where S : Screen<Args.Parcel<P>>, S : Resultable<R>, S : NavigationItem =
+            createResultRouteInternal(
+                destination = destination,
+                inputMapper = { Args.Parcel(inputMapper(it)) },
+                outputMapper = outputMapper
+            )
+    }
+}
+
+// APP SIDE
+
 class SplashScreen(
-    private val theme: Theme,
-    private val createViewModel: (
-        EventsDispatcher<SplashViewModel.EventsListener>
-    ) -> SplashViewModel,
-    private val mainRoute: Route<Args.Parcel<MainInput>>,
-    private val authRoute: Route<Args.Empty>,
-    private val registerRoute: ResultRoute<Args.Empty, RegisterResult>
+    private val mainRoute: Route<String>,
+    private val authRoute: Route<Unit>,
+    private val registerRoute: RouteWithResult<String, String>
 ) : WidgetScreen<Args.Empty>(),
-    SplashViewModel.EventsListener, NavigationItem, ResultRoute.ResultHandler {
+    SplashViewModel.EventsListener, NavigationItem {
+
+    private val registerHandler by registerRouteHandler<String> {
+        if (it == null) {
+            println("no results from screen (moved back without result set)")
+        } else {
+            println("registration result $it")
+        }
+    }
 
     override val navigationBar: NavigationBar = NavigationBar.None
 
     override fun createContentWidget() = TODO()
 
     override fun routeToMain() {
-        mainRoute.route(this, MainInput(""))
+        mainRoute.route(
+            source = this,
+            arg = "token"
+        )
     }
 
     override fun routeToAuth() {
-        authRoute.route(this)
+        authRoute.route(source = this)
     }
 
     override fun routeToRegister() {
-        registerRoute.route(this)
+        registerRoute.route(
+            source = this,
+            arg = "reg",
+            handler = registerHandler
+        )
     }
-
-    override fun onRouteResult(route: ResultRoute<*, *>, result: Any) {
-        when (route) {
-            registerRoute -> {
-                result as RegisterResult
-                println("token is ${result.token}")
-            }
-            else -> {
-                println("fail!")
-            }
-        }
-    }
-
-    @Parcelize
-    data class MainInput(
-        val token: String
-    ) : Parcelable
-
-    @Parcelize
-    data class RegisterResult(
-        val token: String
-    ) : Parcelable
 }
 
 class SplashViewModel : ViewModel() {
@@ -125,72 +232,27 @@ class AuthScreen(
 
 open class RegisterScreen(
     private val theme: Theme
-) : WidgetScreen<Args.Empty>(), NavigationItem {
+) : WidgetScreen<Args.Parcel<RegisterScreen.Input>>(),
+    NavigationItem, Resultable<RegisterScreen.Output> {
+
+    override var screenResult: Output? = null
 
     override val navigationBar: NavigationBar = NavigationBar.None
 
     override fun createContentWidget() = TODO()
+
+    private fun something() {
+        screenResult = Output("test")
+    }
+
+    @Parcelize
+    data class Input(val input: String) : Parcelable
+
+    @Parcelize
+    data class Output(val output: String) : Parcelable
 }
 
 class ARegist(theme: Theme) : RegisterScreen(theme)
-
-class NavigationScreen<T>(
-    initialScreen: TypedScreenDesc<Args.Empty, T>,
-    navigator: Navigator
-) : Screen<Args.Empty>() where T : Screen<Args.Empty>, T : NavigationItem {
-
-}
-
-class Route<Arg : Args>
-
-fun Route<Args.Empty>.route(source: Screen<*>) {
-    println(this)
-    TODO()
-}
-
-fun <IT : Parcelable> Route<Args.Parcel<IT>>.route(source: Screen<*>, data: IT) {
-    println(this)
-    TODO()
-}
-
-class ResultRoute<Arg : Args, T : Parcelable> {
-
-    interface ResultHandler {
-        fun onRouteResult(route: ResultRoute<*, *>, result: Any)
-    }
-}
-
-fun <T : Parcelable, S> ResultRoute<Args.Empty, T>.route(source: S)
-        where S : Screen<*>, S : ResultRoute.ResultHandler {
-    println(this)
-    TODO()
-}
-
-fun <IT : Parcelable, OT : Parcelable, S> ResultRoute<Args.Parcel<IT>, OT>.route(source: S, data: IT)
-        where S : Screen<*>, S : ResultRoute.ResultHandler {
-    println(this)
-    TODO()
-}
-
-class Navigator {
-    fun <Arg : Args, S> createRoute(
-        destination: TypedScreenDesc<Arg, S>
-    ): Route<Arg> where S : Screen<Arg>, S : NavigationItem = TODO()
-
-    fun <T1 : Parcelable, T2 : Parcelable, S> createRoute(
-        destination: TypedScreenDesc<Args.Parcel<T2>, S>,
-        mapping: (T1) -> T2
-    ): Route<Args.Parcel<T1>> where S : Screen<Args.Parcel<T2>>, S : NavigationItem = TODO()
-
-    fun <Arg : Args, R : Parcelable, S> createResultRoute(
-        destination: TypedScreenDesc<Arg, S>
-    ): ResultRoute<Arg, R> where S : Screen<Arg>, S : NavigationItem = TODO()
-}
-
-fun ScreenFactory<Args.Empty, NavigationScreen<*>>.createNavigator(): Navigator {
-    println(this)
-    return Navigator()
-}
 
 class NewApp : BaseApplication() {
     override fun setup(): ScreenDesc<Args.Empty> {
@@ -207,21 +269,26 @@ class NewApp : BaseApplication() {
         }
 
         val navigationScreen = registerScreen(NavigationScreen::class) {
-            val navigator = createNavigator()
+            val router = createRouter()
 
             val splashScreen = registerScreen(SplashScreen::class) {
                 SplashScreen(
-                    theme = theme,
-                    createViewModel = { TODO() },
-                    authRoute = navigator.createRoute(authScreen),
-                    mainRoute = navigator.createRoute(mainScreen) { MainScreen.Arg(token = it.token) },
-                    registerRoute = navigator.createResultRoute(registerScreen)
+                    authRoute = router.createRoute(authScreen),
+                    mainRoute = router.createRoute(
+                        destination = mainScreen,
+                        inputMapper = { MainScreen.Arg(token = it) }
+                    ),
+                    registerRoute = router.createResultRoute(
+                        destination = registerScreen,
+                        inputMapper = { RegisterScreen.Input(it) },
+                        outputMapper = { it.output }
+                    )
                 )
             }
 
             NavigationScreen(
                 initialScreen = splashScreen,
-                navigator = navigator
+                router = router
             )
         }
 
