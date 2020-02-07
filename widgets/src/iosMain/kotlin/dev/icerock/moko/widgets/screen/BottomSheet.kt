d09fd8826d09522b38fbaadd6c8422b40ce1da94
Aleksandr Gusev <agusev@icerockdev.com>
2020-02-07 18:43:24 +0700

    Deleted unused code

--- widgets/src/iosMain/kotlin/dev/icerock/moko/widgets/screen/BottomSheet.kt
+++ widgets/src/iosMain/kotlin/dev/icerock/moko/widgets/screen/BottomSheet.kt
@@ -3,7 +3,6 @@ package dev.icerock.moko.widgets.screen
 import dev.icerock.moko.widgets.core.Widget
 import dev.icerock.moko.widgets.style.view.SizeSpec
 import dev.icerock.moko.widgets.style.view.WidgetSize
-//import platform.FloatingPanel
 import platform.UIKit.UIViewController
 import platform.UIKit.UIView
 
@@ -12,9 +11,5 @@ var floatPanelShow: ((vc: UIViewController, body: UIView) -> Unit)? = null
 actual fun Screen<*>.showBottomSheet(
     content: Widget<WidgetSize.Const<SizeSpec.AsParent, SizeSpec.WrapContent>>
 ) {
-    // floatPanelShow(this.viewController, content.buildView().view)
     floatPanelShow?.let { it(this.viewController, content.buildView(viewFactoryContext = this.viewController).view) }
 }
-
-// а вот это вызвать на нативе. На каком контроллере отобразить эту вьюху
-//BottomSheetKt.floatPanelShow = { }
