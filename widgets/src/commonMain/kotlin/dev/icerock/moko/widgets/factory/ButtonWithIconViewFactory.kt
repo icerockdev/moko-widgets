467c4838ead2ff401d8c3b9407e46845e78da33f
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-02-10 00:12:14 +0700

    #27 android implementation and fix of iOS

--- widgets/src/commonMain/kotlin/dev/icerock/moko/widgets/factory/ButtonWithIconViewFactory.kt
+++ widgets/src/commonMain/kotlin/dev/icerock/moko/widgets/factory/ButtonWithIconViewFactory.kt
@@ -1,9 +1,10 @@
 /*
- * Copyright 2019 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2020 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package dev.icerock.moko.widgets.factory
 
+import dev.icerock.moko.resources.ImageResource
 import dev.icerock.moko.widgets.ButtonWidget
 import dev.icerock.moko.widgets.core.Image
 import dev.icerock.moko.widgets.core.Value
@@ -14,15 +15,21 @@ import dev.icerock.moko.widgets.style.view.MarginValues
 import dev.icerock.moko.widgets.style.view.PaddingValues
 import dev.icerock.moko.widgets.style.view.TextStyle
 
-expect class ButtonWithImageViewFactory(
+expect class ButtonWithIconViewFactory(
     background: StateBackground? = null,
     textStyle: TextStyle? = null,
     isAllCaps: Boolean? = null,
     padding: PaddingValues? = null,
     margins: MarginValues? = null,
     androidElevationEnabled: Boolean? = null,
-    icon: Value<Image>,
-    titleLeftIconRight: Boolean? = null,
-    contentIndentFromBorder: Double? = null
+    iconGravity: IconGravity? = null,
+    iconPadding: Float? = null,
+    icon: ImageResource
 ) : ViewFactory<ButtonWidget<out WidgetSize>>
 
+enum class IconGravity {
+    START,
+    END,
+    TEXT_START,
+    TEXT_END
+}
