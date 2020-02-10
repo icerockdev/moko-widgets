7a448ef66b72554d1acccf6b131222c4ed8b22a1
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-02-10 18:08:51 +0700

    #9 android implementation of navbar styles

--- widgets/src/androidMain/kotlin/dev/icerock/moko/widgets/screen/navigation/NavigationScreen.kt
+++ widgets/src/androidMain/kotlin/dev/icerock/moko/widgets/screen/navigation/NavigationScreen.kt
@@ -5,13 +5,24 @@
 package dev.icerock.moko.widgets.screen.navigation
 
 import android.content.Context
+import android.graphics.Typeface
+import android.os.Build
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.AbsoluteSizeSpan
+import android.text.style.StyleSpan
+import android.text.style.TypefaceSpan
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.FrameLayout
 import android.widget.LinearLayout
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
@@ -22,8 +33,11 @@ import dev.icerock.moko.widgets.screen.FragmentNavigation
 import dev.icerock.moko.widgets.screen.Screen
 import dev.icerock.moko.widgets.screen.TypedScreenDesc
 import dev.icerock.moko.widgets.screen.unsafeSetScreenArgument
+import dev.icerock.moko.widgets.style.view.FontStyle
 import dev.icerock.moko.widgets.utils.ThemeAttrs
 import dev.icerock.moko.widgets.utils.dp
+import dev.icerock.moko.widgets.utils.sp
+
 
 actual abstract class NavigationScreen<S> actual constructor(
     private val initialScreen: TypedScreenDesc<Args.Empty, S>,
@@ -50,11 +64,6 @@ actual abstract class NavigationScreen<S> actual constructor(
         childFragmentManager.addOnBackStackChangedListener {
             val haveBack = childFragmentManager.backStackEntryCount > 0
             backPressedCallback.isEnabled = haveBack
-            toolbar?.navigationIcon = if (haveBack) {
-                ThemeAttrs.getToolBarUpIndicator(requireContext())
-            } else {
-                null
-            }
         }
         childFragmentManager.registerFragmentLifecycleCallbacks(
             object : FragmentManager.FragmentLifecycleCallbacks() {
@@ -113,11 +122,7 @@ actual abstract class NavigationScreen<S> actual constructor(
             id = android.R.id.content
         }
         val toolbar = Toolbar(context).apply {
-            setBackgroundColor(ThemeAttrs.getPrimaryColor(context))
             ViewCompat.setElevation(this, 4.dp(context).toFloat())
-            setNavigationOnClickListener {
-                childFragmentManager.popBackStack()
-            }
         }
 
         this.toolbar = toolbar
@@ -177,20 +182,100 @@ actual abstract class NavigationScreen<S> actual constructor(
     private fun updateNavigation(fragment: Fragment) {
         fragment as NavigationItem
 
+        val toolbar = this.toolbar ?: return
+        val context = requireContext()
+
         when (val navBar = fragment.navigationBar) {
             NavigationBar.None -> {
-                toolbar?.visibility = View.GONE
+                toolbar.visibility = View.GONE
             }
             is NavigationBar.Normal -> {
-                toolbar?.visibility = View.VISIBLE
-                toolbar?.title = navBar.title.toString(requireContext())
-                if (navBar.styles?.backgroundColor != null) {
-                    toolbar?.setBackgroundColor(navBar.styles.backgroundColor.argb.toInt())
+                toolbar.visibility = View.VISIBLE
+
+                val title = navBar.title.toString(context)
+                toolbar.title = SpannableString(title).apply {
+                    val size = navBar.styles?.textStyle?.size?.toFloat()?.sp(context)
+                    if(size != null) {
+                        val sizeSpan = AbsoluteSizeSpan(size.toInt())
+                        setSpan(sizeSpan, 0, title.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                    }
+
+                    val fontStyle = navBar.styles?.textStyle?.fontStyle
+                    if(fontStyle != null) {
+                        val style = when(fontStyle) {
+                            FontStyle.BOLD -> Typeface.BOLD
+                            FontStyle.MEDIUM -> Typeface.NORMAL
+                        }
+                        val styleSpan = StyleSpan(style)
+                        setSpan(styleSpan, 0, title.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                    }
                 }
+
+                val bgColor = if (navBar.styles?.backgroundColor != null) {
+                    navBar.styles.backgroundColor.argb.toInt()
+                } else {
+                    ThemeAttrs.getPrimaryColor(context)
+                }
+                toolbar.setBackgroundColor(bgColor)
+                setStatusBarColor(bgColor)
+
+                val fallbackTintColor = ThemeAttrs.getControlNormalColor(context)
+
+                val tintColor = navBar.styles?.tintColor?.argb?.toInt() ?: fallbackTintColor
+
+                toolbar.setTitleTextColor(tintColor)
+                toolbar.overflowIcon?.also { DrawableCompat.setTint(it, tintColor) }
+
                 if (navBar.styles?.textStyle?.color != null) {
-                    toolbar?.setTitleTextColor(navBar.styles.textStyle.color.argb.toInt())
+                    toolbar.setTitleTextColor(navBar.styles.textStyle.color.argb.toInt())
+                } else {
+                    toolbar.setTitleTextColor(fallbackTintColor)
+                }
+
+                val backBtn = navBar.backButton
+                if (backBtn != null) {
+                    toolbar.navigationIcon = ContextCompat.getDrawable(context, backBtn.icon.drawableResId)
+                    toolbar.setNavigationOnClickListener {
+                        backBtn.action()
+                    }
+                } else {
+                    toolbar.navigationIcon = if (childFragmentManager.backStackEntryCount > 0) {
+                        ThemeAttrs.getToolBarUpIndicator(requireContext())
+                    } else {
+                        null
+                    }
+                    toolbar.setNavigationOnClickListener {
+                        childFragmentManager.popBackStack()
+                    }
+                }
+                toolbar.navigationIcon?.also { DrawableCompat.setTint(it, tintColor) }
+
+                val actions = navBar.actions
+                if (actions != null) {
+                    actions.forEach { barBtn ->
+                        val item = toolbar.menu.add("$barBtn")
+                        item.icon = ContextCompat.getDrawable(context, barBtn.icon.drawableResId)
+                        DrawableCompat.setTint(item.icon, tintColor)
+                        item.setOnMenuItemClickListener {
+                            barBtn.action()
+                            true
+                        }
+                        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+                    }
+                } else {
+                    toolbar.menu.clear()
                 }
             }
+        }
+    }
+
+    private fun setStatusBarColor(bgColor: Int) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) return
+
+        with(requireActivity().window) {
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            statusBarColor = bgColor
         }
     }
 
