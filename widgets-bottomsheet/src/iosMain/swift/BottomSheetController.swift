3eb797af86ca07457ea57f73f693200bef3c5b72
Andrey Tchernov <tchernov@icerockdev.com>
2020-02-11 12:09:48 +0700

    #73 Show bottom sheet only in half mode

--- widgets-bottomsheet/src/iosMain/swift/BottomSheetController.swift
+++ widgets-bottomsheet/src/iosMain/swift/BottomSheetController.swift
@@ -5,13 +5,42 @@
 import UIKit
 import FloatingPanel
 
-@objc public class BottomSheetController: NSObject {
+@objc public class BottomSheetController: NSObject, FloatingPanelControllerDelegate {
+  
+  private var floatLoayout: FloatingPanelLayout?
+  
   @objc public func show(onViewController vc: UIViewController, withContent view: UIView) {
     let contentVC = UIViewController()
     contentVC.view = view
     let fpc = FloatingPanelController()
     fpc.set(contentViewController: contentVC)
+    fpc.delegate = self
     fpc.isRemovalInteractionEnabled = true
+    floatLoayout = BottomSheetLayout(preferredHeight: view.sizeThatFits(UIScreen.main.bounds.size).height)
+    
     vc.present(fpc, animated: true, completion: nil)
   }
+  
+  public func floatingPanel(_ vc: FloatingPanelController, layoutFor newCollection: UITraitCollection) -> FloatingPanelLayout? {
+    return floatLoayout
+  }
+}
+
+class BottomSheetLayout: FloatingPanelLayout {
+  var initialPosition: FloatingPanelPosition = .half
+  private var preferredHeight: CGFloat
+  init(preferredHeight: CGFloat) {
+    self.preferredHeight = preferredHeight
+  }
+  
+  func insetFor(position: FloatingPanelPosition) -> CGFloat? {
+    switch (position) {
+    case .half: return preferredHeight
+    case .full: return 0
+    case .tip: return 0
+    case .hidden: return nil
+    }
+  }
+  
+  var supportedPositions: Set<FloatingPanelPosition> = [.half, .hidden, .tip]
 }
