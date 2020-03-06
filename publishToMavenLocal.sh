bc52df4aba021a9553651265ba46730e18cb1b83
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-03-06 18:15:09 +0700

    update readme

--- publishToMavenLocal.sh
+++ publishToMavenLocal.sh
@@ -1,3 +1,4 @@
 ./gradlew -PpluginPublish publishPluginPublicationToMavenLocal
 ./gradlew -PlibraryPublish :widgets:publishToMavenLocal
+(cd sample/ios-app && pod install)
 ./gradlew -PlibraryPublish :widgets-flat:publishToMavenLocal :widgets-bottomsheet:publishToMavenLocal :widgets-sms:publishToMavenLocal :widgets-datetime-picker:publishToMavenLocal :widgets-collection:publishToMavenLocal
