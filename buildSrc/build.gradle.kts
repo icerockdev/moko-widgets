9602d97e3da677643ae34a2014163d4e8876527b
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-03-28 23:31:09 +0700

    update kotlin to 1.3.71

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,8 +14,8 @@ repositories {
 }
 
 dependencies {
-    implementation("dev.icerock:mobile-multiplatform:0.6.0")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.70")
+    implementation("dev.icerock:mobile-multiplatform:0.6.1")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.71")
     implementation("com.android.tools.build:gradle:3.6.1")
 }
 
