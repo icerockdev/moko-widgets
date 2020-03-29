af3a54ebf555c64f53af77626f1e0aa078522bcf
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-03-29 21:52:38 +0700

    update to gradle 6.2.2 (as on github runners)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,28 @@
  * Copyright 2019 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
  */
 
+buildscript {
+    repositories {
+        mavenLocal()
+
+        jcenter()
+        google()
+
+        maven { url = uri("https://dl.bintray.com/kotlin/kotlin") }
+        maven { url = uri("https://kotlin.bintray.com/kotlinx") }
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        maven { url = uri("https://dl.bintray.com/icerockdev/plugins") }
+
+        maven { url = uri("https://dl.bintray.com/icerockdev/plugins-dev") }
+    }
+
+    dependencies {
+        Deps.plugins.values.forEach {
+            classpath(it)
+        }
+    }
+}
+
 allprojects {
     repositories {
         mavenLocal()
