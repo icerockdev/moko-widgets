f9517ea9e4926865d5da0e1598b630e68f365d8e
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2020-11-11 19:32:44 +0700

    #284 down android gradle plugin for IDEA

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 dependencies {
     implementation("dev.icerock:mobile-multiplatform:0.8.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
-    implementation("com.android.tools.build:gradle:4.1.0")
+    implementation("com.android.tools.build:gradle:4.0.1")
 }
 
 val mokoWidgetsVersion: String = when (properties.containsKey("devPublish")) {
